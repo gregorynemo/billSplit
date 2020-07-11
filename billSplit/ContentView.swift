93b76b308fa0108e63105db25da9c33295ef49f6
Gregory Nemo II <gregory.nemo.ii@gmail.com>
2020-07-11 11:41:49 -0700

    adding and array of tipPercentages to be selected from a picker. now looking at tipPercentage = 2, that would be a 20% tip, as that is its index number

--- billSplit/ContentView.swift
+++ billSplit/ContentView.swift
@@ -13,6 +13,8 @@ struct ContentView: View {
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 2
     
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
        Text("Hello World.")
     }
