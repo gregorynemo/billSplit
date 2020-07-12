4cf4f1183dbc975a4a03c265cd9aa5150b97ba2c
Gregory Nemo II <gregory.nemo.ii@gmail.com>
2020-07-12 12:36:50 -0700

    added functionality to picker, to allow input of 5+ users, and revealing a textField when it is selected. Input has 2-way binding

--- billSplit/ContentView.swift
+++ billSplit/ContentView.swift
@@ -18,13 +18,21 @@ extension View {
 
 struct ContentView: View {
     @State private var checkAmount = ""
+    @State private var morePeople = ""
     @State private var numberOfPeople = 0
     @State private var tipPercentage = 2
     
     let tipPercentages = [10, 15, 20, 25, 0]
+    let numberOfPeoples = [1, 2, 3, 4, 5]
     
     var totalPerPerson: (Double, Double) {
-        let peopleCount = Double(numberOfPeople + 1)
+        var peopleCount: Double = 0
+        if numberOfPeople != 5 {
+            peopleCount = Double(numberOfPeople + 1)
+        } else {
+            peopleCount = Double(morePeople) ?? 0
+        }
+            
         let tipSelection = Double(tipPercentages[tipPercentage])
         let orderAmount = Double(checkAmount) ?? 0
         
@@ -48,12 +56,22 @@ struct ContentView: View {
                 
                 Section (header: Text("How many people?")) {
                     Picker("Number of People", selection: $numberOfPeople) {
-                        ForEach(1 ..< 6) {
-                            Text("\($0)")
+                        ForEach(0 ..< numberOfPeoples.count) {
+                            if (self.numberOfPeoples[$0] < 5) {
+                                Text("\(self.numberOfPeoples[$0])")
+                            } else {
+                                Text("5(+)")
+                            }
                         }
                     }
                 .pickerStyle(SegmentedPickerStyle())
                 }
+                if numberOfPeople == 4 {
+                    Section {
+                        TextField("Enter # of people", text: $morePeople)
+                            .keyboardType(.decimalPad)
+                        }
+                    }
                 
                 Section (header: Text("How much tip do you want to leave?")) {
                     Picker("Tip Percentage", selection: $tipPercentage) {
